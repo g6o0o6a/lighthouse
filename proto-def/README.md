0b0052d5bc2d56af0a6450f7db4066ae2eb6e402
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-10-04 13:42:02 -0700

    Struct details in proto, added python to convert sample into proto and do a round trip run.

--- proto-def/README.md
+++ proto-def/README.md
@@ -2,3 +2,7 @@ compile proto python definitions
 ```bash
     protoc --python_out=./ lighthouse-response.proto
 ```
+run python round trip test
+```bash
+    python proto_lhr_validator.py
+```
--- proto-def/proto_lhr_validator.py
+++ proto-def/proto_lhr_validator.py
@@ -1,20 +1,71 @@
+import os
 import sys
 import json
 import lighthouse_response_pb2 as lhr_pb2
 from google.protobuf.json_format import Parse, MessageToJson
 
+cleaned_name = 'lhr_cleaned.json'
+
+def clean():
+    try:
+        os.remove(cleaned_name)
+    except OSError:
+        pass
+
+def make_cleaned_sample_json(output_filename='lhr_cleaned.json'):
+    with open('../lighthouse-core/test/results/sample_v2.json', 'r') as f:
+        data = json.load(f)
+
+    # convert scoreDisplayMode
+    audits = data["audits"]
+
+    for audit_id, audit in audits.items():
+        if "scoreDisplayMode" in audit:
+            if audit["scoreDisplayMode"] == "not-applicable":
+                audit["scoreDisplayMode"] = "not_applicable"
+
+    # remove rawValue
+    for audit_id, audit in audits.items():
+        if "rawValue" in audit:
+            del audit["rawValue"]
+
+    # clean up displayValue to be only strings
+    for audit_id, audit in audits.items():
+        if "displayValue" in audit:
+            if type(audit["displayValue"]) is list:
+                new_list = []
+                for item in audit["displayValue"]:
+                    if type(audit["displayValue"]) is not unicode:
+                        item = str(item)
+                    new_list.append(item)
+                audit["displayValue"] = new_list
+            else:
+                audit["displayValue"] = [audit["displayValue"]]
+
+    # convert i18n -> EXPERIMENTALI18n
+    data['EXPERIMENTALI18n'] = data['i18n']
+    del data['i18n']
+    
+    with open(output_filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+clean()
+
+make_cleaned_sample_json()
 
 # get json file
-with open('lhr_example_pruned.json', 'r') as f:
+with open('lhr_cleaned.json', 'r') as f:
     data = json.load(f)
 
 # convert string to proto object
 lhr = lhr_pb2.LighthouseResponse()
-
 Parse(json.dumps(data), lhr)
 
 # write proto object to console
-print(lhr)
+json_lhr = json.loads(MessageToJson(lhr))
 
 # validate naive truth
-print(json.loads(MessageToJson(lhr)) == data) # is false b/c defaults!!!
+print(json_lhr == data) # is false b/c defaults!!!
+
+with open('lhr_round_trip.json', 'w') as f:
+    json.dump(json.loads(MessageToJson(lhr)), f, indent=4)
